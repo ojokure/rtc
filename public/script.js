f789f12a5598bee97a1586e18c32c4f418d1d9a7
oladimeji-ojo <ojokuredim@gmail.com>
2020-09-11 14:53:36 +0100

    created video mediaElement and passing stream to it

--- public/script.js
+++ public/script.js
@@ -1,9 +1,25 @@
 const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
 
 const RoomPeer = new Peer(null, {
   host: "/",
   port: "3031",
 });
+
+const videoChat = document.createElement("video");
+videoChat.muted = true;
+
+let constraints = {
+  video: true,
+  audio: true,
+};
+
+navigator.mediaDevices
+  .getUserMedia(constraints)
+  .then((stream) => {
+    addVideoStream(videoChat, stream);
+  })
+  .catch((err) => console.log(err));
 
 RoomPeer.on("open", (id) => {
   socket.emit("join-video-chat", ROOM_ID, id);
@@ -12,3 +28,12 @@ RoomPeer.on("open", (id) => {
 socket.on("new-user-joined", (userId) => {
   console.log("new user connected", userId);
 });
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+
+  videoGrid.append(video);
+}
